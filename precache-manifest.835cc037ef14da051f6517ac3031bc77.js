9d3404a8cd9df43d71ed0fd21bde78210a9303f1
Manourez <manourez@hotmail.fr>
2020-09-18 20:55:11 +0000

    added files

---
+++ precache-manifest.835cc037ef14da051f6517ac3031bc77.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902c40f2848300d6e8f6b38e6e3f0544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c91e6878bd7248097a10",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "76bc81c3713460c6b0b3",
+    "url": "/static/css/main.b0c640b9.chunk.css"
+  },
+  {
+    "revision": "c91e6878bd7248097a10",
+    "url": "/static/js/2.3d4fd0c8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3d4fd0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bc81c3713460c6b0b3",
+    "url": "/static/js/main.c022bbc2.chunk.js"
+  },
+  {
+    "revision": "397b0ed61f65304b6f1f",
+    "url": "/static/js/runtime-main.39823f70.js"
+  },
+  {
+    "revision": "fff018d427dc3a90432ba52da9bd090c",
+    "url": "/static/media/addcircle.fff018d4.svg"
+  },
+  {
+    "revision": "cb02a208314ec4d6cee150ca66085dd4",
+    "url": "/static/media/calendar.cb02a208.svg"
+  },
+  {
+    "revision": "68f9783efe90d9d9ec0d6cff132a9c67",
+    "url": "/static/media/camp.68f9783e.jpg"
+  },
+  {
+    "revision": "c5f9e3d91aec9ccf2e97b22ec62d9110",
+    "url": "/static/media/couche.c5f9e3d9.jpg"
+  },
+  {
+    "revision": "d2cc9893a4f5ac4fce85232e0b3dd42d",
+    "url": "/static/media/header-back.d2cc9893.jpg"
+  },
+  {
+    "revision": "a82bb224af536c3b61c9aa0ef2aa103a",
+    "url": "/static/media/manufacturing.a82bb224.png"
+  },
+  {
+    "revision": "9bb806eab3ff8b078b9cd04384b945e1",
+    "url": "/static/media/mer.9bb806ea.jpg"
+  },
+  {
+    "revision": "9b444e359c1a5b8a9d7221f1dc8d8e02",
+    "url": "/static/media/messenger.9b444e35.png"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "/static/media/search.db5d8122.svg"
+  },
+  {
+    "revision": "5b08a89d2dd3351ce4ef110d2909cb23",
+    "url": "/static/media/send.5b08a89d.svg"
+  }
+]);
